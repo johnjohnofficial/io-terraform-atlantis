83353efede8cbbdcc461e5b696c258e17d73b1bb
Joao Ferreira <jferreira.ba@gmail.com>
2019-07-25 17:12:21 -0300

    [*] sample 2 - Modifiy infra.

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Configure the AWS Provider
+## https://www.terraform.io/docs/providers/aws/index.html
 provider "aws" {
   version = "~> 2.0"
   region  = "us-east-2"
@@ -6,9 +6,10 @@ provider "aws" {
   secret_key = "q+jLRCR/bISeAsm3yX0q1ee6G8GjVcNkqyiZStsy"
 }
 
+## https://www.terraform.io/docs/providers/aws/r/instance.html
 resource "aws_instance" "sample" {
   ami           = "ami-0986c2ac728528ac2"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"  ## https://aws.amazon.com/pt/ec2/instance-types/
 }
 
 output "dns_public" {
